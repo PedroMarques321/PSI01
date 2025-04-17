3ec08b9da09846b029b6970518a84f7acbec735e
PedroMarques321 <pedromarques2881997@gmail.com>
2025-04-17 18:30:55 +0100

    favicon, dashboard e profile init

--- PSI01/src/app/app.module.ts
+++ PSI01/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManagementComponent } from './management/management.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManagementComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
