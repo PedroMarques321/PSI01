32d6563b572157a99363a0094347c6d84da6608e
PedroMarques321 <pedromarques2881997@gmail.com>
2025-04-21 17:23:13 +0100

    driver interface and backend model

---
+++ frontend/src/app/driver.ts
@@ -0,0 +1,9 @@
+export interface Driver {
+    _id: String | null,
+    nome: string,
+    genero: string,
+    nif: string,
+    carta_de_conducao: string,
+    codigo_postal: string,
+    ano_de_nascimento: Date
+}
