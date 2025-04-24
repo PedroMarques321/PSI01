f26d5283a97f5541bbc49956b67aeae2be928ead
Pedro Marques <pedromarques2881997@gmail.com>
2025-04-24 18:50:25 +0100

    Apagado codigo inutil, Sprint1

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,22 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// Require controller modules.
-const herocontroller = require("../controllers/heroController");
-
-// Hero endpoints
-router.get("/heroes", herocontroller.heroesGetAll);
-router.get("/hero/:id", herocontroller.heroGetId);
-router.post("/hero", herocontroller.hero_create_post);
-router.delete("/hero/:id", herocontroller.hero_delete);
-router.put("/hero/:id", herocontroller.heroPut);
-
-const petcontroller = require("../controllers/petController");
-
-// Pet endpoints
-router.get("/pets", petcontroller.petsGetAll);
-router.get("/pet/:id", petcontroller.petGetId);
-
 const taxicontroller = require("../controllers/taxiController");
 
 //Taxi endpoints
